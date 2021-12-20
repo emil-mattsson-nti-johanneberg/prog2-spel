bdc86e30530db8becefc8bfba30ad4a876cd1e76
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2021-12-20 10:21:06 +0100

    lektion 20/12-2021
    fixade lite till så att allt funkar. spelet är typ klart nu

--- game.rb
+++ game.rb
@@ -7,28 +7,23 @@ class Entitet
       @y = y
       @sprite = sprite
     end
-  
     def width
       @sprite.width
     end
-  
     def height
       @sprite.height
     end
-  
     def draw
       @sprite.draw x, y, 0
     end
-
     def collides? (ball)
-        @aux = ball.x < @x+width && ball.x+ball.width > @x && ball.y < @y+height && ball.y+ball.height > @y
-        if @aux
-          ball.vel_y = -ball.vel_y
+            @aux = ball.x < @x+width && ball.x+ball.width > @x && ball.y < @y+height && ball.y+ball.height > @y
+            if @aux
+            ball.vel_y = -ball.vel_y
+            end
+        
+            return @aux
         end
-           
-        return @aux
-    end
-
 end
 
 module ZOrder
@@ -47,7 +42,7 @@ class Brick < Entitet
 end
 
 class Ball < Entitet
-    attr_accessor :vel_y, :vel_x
+    attr_accessor :vel_x, :vel_y, :x, :y
     attr_reader :lifes, :score
     def initialize x, y, window
         @window = window
@@ -87,7 +82,7 @@ class Ball < Entitet
 end
 
 class Player < Entitet
-    attr_accessor :x, :lifes
+    attr_accessor :lifes, :score
     def initialize x, y, window
         @image = Gosu::Image.new(window, "media/paddle.png", true)
         super x, y, @image
@@ -95,7 +90,7 @@ class Player < Entitet
         @score = 0
     end
     def draw
-        @image.draw_rot(@x, 420)
+        @image.draw(@x, @y, 0)
     end
     def move_left
         @x -= 5
@@ -122,20 +117,20 @@ class Game < Gosu::Window
         8.times { |i| @blocks.push(Brick.new(self, 82*i,110)) }
         8.times { |i| @blocks.push(Brick.new(self, 82*i,140)) }
         8.times { |i| @blocks.push(Brick.new(self, 82*i,170)) }
-        8.times { |i| @blocks.push(Brick.new(self, 82*i,200)) }
     end
 
     def draw
         @ball.draw
         @blocks.each { |block| block.draw }
         @player.draw
-        @font.draw_text("LIFES: #{@lifes}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+        @font.draw_text("LIFE: #{@lifes}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
         @font.draw_text("SCORE: #{@ball.score}", 10, 30, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
     end
 
     def update
         @ball.update
         @ball.collect_blocks(@blocks)
+
         if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
             @player.move_left
         end
@@ -143,27 +138,19 @@ class Game < Gosu::Window
             @player.move_right
         end
 
-        if @ball.y == 400 
-            if @ball.x < @player.x+50 && @ball.x > @player.x-50
-                @ball.vel_y *= -1
+        if @player.collides?(@ball)
+            @ball.vel_x *= 1.05
+            @ball.vel_y *= 1.05
+            if @blocks.length == 0
+                create_blocks
+                @ball.vel_x *= 1.1
                 @ball.vel_y *= 1.1
             end
         end
 
-        #if @player.collides?(@ball)
-     # @ball.vel_x *= 1.1
-      #@ball.vel_y *= 1.1
-      #if @blocks.length == 0
-        #create_blocks
-        #@ball.vel_x *= 1.25
-        #@ball.vel_y *= 1.25
-      #end
-    #end
-
         if @ball.y > 640 and button_down? Gosu::KB_SPACE
             @lifes -= 1
-            @ball.reset
-            @player.reset
+            @ball.reset @player.x, @player.y 
         end
     end
     def button_down(id)
